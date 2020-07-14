38ca494eda33995c0ef0b59e3d221f4baa6def00
Jan Van Balen <jan.vanbalen@uantwerpen.be>
2020-07-14 15:52:49 +0200

    tidy

--- draw.py
+++ draw.py
@@ -30,47 +30,59 @@ def rand_crop(x, dims=(9, 7), scale=1.0, resolution=54):
 
 def draw_card(text, out_path=None, dims=(9, 7), std_xy=1.0, text_size=20, bg_path=BG_PATH, max_angle=10, dy=0.45):
 
-    # np.random.seed(1988)
-    w, h = dims
-    facecolor=[1, 1, .98]
-    x0 = std_xy * np.random.randn()
+    # more parameters
+    text_color = [.2, .2, .3]
+    card_color = [1, 1, .98]
+
+    # random parameters
+    scale = np.random.rand() + 1  # 'zoom' level
+    x0 = std_xy * np.random.randn()  # center of card
     y0 = std_xy * np.random.randn()
-    scale = np.random.rand() + 1
+
+    # derived parameters
+    w, h = dims
     text_size /= scale
-    
     xlim = [-scale * w/2, scale * w/2]
     ylim = [-scale * h/2, scale * h/2]
+
+    # draw background
     fig, ax = plt.subplots(1, 1, figsize=(w, h))
     if bg_path is not None:
         im = np.array(Image.open(bg_path), dtype=np.uint8)
         im = rand_crop(im, dims=dims, scale=scale)
         ax.imshow(im, extent=xlim + ylim, origin='upper')
     
-    lines = textwrap.fill(text, 42).split('\n')
-    n_lines = sum(len(l) for l in lines) / max(len(l) for l in lines)
+    # get rotation transform
     rand_angle = max_angle * (2 * np.random.rand() - 1)
-    print(f'box: x={x0:.3f}, y={y0:.3f}')
-    rect = patches.FancyBboxPatch((x0 - w/2,y0 - h/2), w/1, h/1, linewidth=0, facecolor=facecolor,
-                                  boxstyle="round,pad=0.0,rounding_size=0.5")
     transform = mpl.transforms.Affine2D().rotate_deg(rand_angle) + ax.transData
+
+    # draw card
+    print(f'box: x={x0:.3f}, y={y0:.3f}')
+    rect = patches.FancyBboxPatch((x0 - w/2,y0 - h/2), w/1, h/1, linewidth=0, facecolor=card_color,
+                                  boxstyle="round,pad=0.0,rounding_size=0.5")
     rect.set_transform(transform)
     ax.add_patch(rect);
+
+    # draw text
     y0 = y0 + dy * (n_lines - 1) / 2
+    lines = textwrap.fill(text, 42).split('\n')
+    n_lines = sum(len(l) for l in lines) / max(len(l) for l in lines)
     for i, line in enumerate(lines):
         y = y0 - dy * i
         if len(lines) == 1:
             x = x0
             ha = 'center'
         else:
-            # left-align multi-line prompts
+            # left-align multi-line prompts, as in the original card set
             x = x0 - w / 2.8
             ha = 'left'
         print(f'text: x={x:.3f}, y={y:.3f}')
         if os.path.exists(FONT_PATH):
-            ax.text(x, y, line, color=[.2, .2, .3], fontproperties=fm.FontProperties(fname=FONT_PATH, size=text_size),
+            # if we're on Heroku, use its closest approximation to Helvetica
+            ax.text(x, y, line, color=text_color, fontproperties=fm.FontProperties(fname=FONT_PATH, size=text_size),
                     ha=ha, rotation=rand_angle, rotation_mode='anchor', transform=transform)
         else:
-            ax.text(x, y, line, color=[.2, .2, .3], name='helvetica', size=text_size, 
+            ax.text(x, y, line, color=text_color, name='helvetica', size=text_size, 
                     ha=ha, rotation=rand_angle, rotation_mode='anchor', transform=transform)
 
     ax.axis('off');
